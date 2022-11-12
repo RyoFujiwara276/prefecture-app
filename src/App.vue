88831bc092dd8d79955c2ba7f841129835b256f7
RyoFujiwara <user@Ryo-Fujiwara-Macbook-13-inch.local>
2022-11-12 17:52:52 +0900

    change from mounted to method

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,16 @@ export default {
       prefectures: null,
     };
   },
-  mounted() {
-    var prefectures_url =
+  methods: {
+    getPrefData() {
+      var prefectures_url =
       "https://opendata.resas-portal.go.jp/api/v1/prefectures";
     axios
       .get(prefectures_url, {
         headers: { "X-API-KEY": "8OSsW1I7j1SPrT1Ne3zZmLpXfPd6cPjRtGA3WuHM" },
       })
       .then((response) => (this.prefectures = response.data.result));
-  },
+    }
+  }
 };
 </script>
