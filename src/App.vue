5752e46cf3c0367dddf8a491b0a79233797a7aad
RyoFujiwara <user@Ryo-Fujiwara-Macbook-13-inch.local>
2022-11-13 18:14:05 +0900

    add the max value to the x axis

--- src/App.vue
+++ src/App.vue
@@ -31,32 +31,27 @@ export default {
           text: "結果"
         },
         legend: {
-          layout: "horizontal"
+          layout: "horizontal",
         },
         xAxis: {
           title: {
             text: "Year (年度)"
           },
           categories: [
-            1950,
-            1955,
-            1960,
-            1965,
-            1970,
-            1975,
             1980,
-            1985,
+            // 1985,
             1990,
-            1995,
+            // 1995,
             2000,
-            2005,
+            // 2005,
             2010,
-            2015,
+            // 2015,
             2020,
-            2025,
+            // 2025,
             2030,
-            2035
-          ]
+            // 2035
+          ],
+          max: 5
         },
         yAxis: {
           title: {
